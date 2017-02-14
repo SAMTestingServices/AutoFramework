64f63c16b9c40e1cdf9f12df310654701e5f02da
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-14 11:41:09 +0000

    Updated the list of test environments

--- DRFosterAutomationFramework/Common/ListofSiteURLs.cs
+++ DRFosterAutomationFramework/Common/ListofSiteURLs.cs
@@ -2,14 +2,19 @@
 {
     public class AddressList
     {
-        public static string BaseURLLive
-        {
-            get { return  "https://my.drfoster.co.uk/"; }
-        }
-        public static string Testing005URL
+        public static string embedTestUrl
         {
             get { return "http://testing05:50000/mbed"; }
         }
 
+        public static string myDrFosterTestUrl
+        { 
+           get { return "http://testing05";  }         
+        }
+
+        public static string BaseURLLive
+        {
+            get { return "https://my.drfoster.co.uk/"; }
+        }
     }
 }
