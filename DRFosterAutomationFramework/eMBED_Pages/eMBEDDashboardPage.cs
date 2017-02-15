515b0bd25f4586cf25c5e1bb76b3618afaa38cc1
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-15 11:46:12 +0000

    Added new files for eMBED

---
+++ DRFosterAutomationFramework/eMBED_Pages/eMBEDDashboardPage.cs
@@ -0,0 +1,28 @@
+﻿using DRFosterAutomationFramework.Common;
+using DRFosterAutomationFramework.Helpers;
+using OpenQA.Selenium;
+using System;
+
+namespace DRFosterAutomationFramework.eMBED_Pages
+{
+   public class eMBEDDashboardPage
+    {
+        public static void validateMainNavigation()
+        {
+            var mainNavigation = Driver.Instance.FindElement(By.Id("main-navigation"));
+
+            if (mainNavigation != null) // TODO: change this to an assert of the ID, check what it actually returns first
+            {
+                Console.WriteLine("I found the main-navigation element by its ID");
+            }
+            else
+            {
+                Console.WriteLine("The Main Navigation element was NOT located on the page");
+                var mainNavigationException = $"I expected to find the main-navigation element, but instead I got {mainNavigation} ";
+                TakeScreenshot.SaveScreenshot();
+                throw new Exception(mainNavigationException);
+            }
+        }
+
+    }
+}
--- DRFosterAutomationFramework/eMBED_Pages/eMBEDLandingPage.cs
+++ DRFosterAutomationFramework/eMBED_Pages/eMBEDLandingPage.cs
@@ -1,11 +1,10 @@
 ﻿using DRFosterAutomationFramework.Common;
 using DRFosterAutomationFramework.Helpers;
-using OpenQA.Selenium;
 using System;
 
 namespace DRFosterAutomationFramework.eMBED_Pages
 {
-    class eMBEDLandingPage
+    public class eMBEDLandingPage
     {
         public static void GoToEmbedHomePage()
         {
@@ -32,23 +31,6 @@ namespace DRFosterAutomationFramework.eMBED_Pages
                 var embedHomePageException = $"I expected ' { expectedPageTitle } ', but I saw ' { actualPageTitle } '.";
                 TakeScreenshot.SaveScreenshot();
                 throw new Exception(embedHomePageException);
-            }
-        }
-        
-        public static void validateMainNavigation()
-        {
-            var mainNavigation = Driver.Instance.FindElement(By.Id("main-navigation"));
-            
-            if (mainNavigation != null) // TODO: change this to an assert of the ID, check what it actually returns first
-            {
-                Console.WriteLine("I found the main-navigation element by its ID");
-            }
-            else
-            {
-                Console.WriteLine("The Main Navigation element was NOT located on the page");
-                var mainNavigationException = $"I expected to find the main-navigation element, but instead I got {mainNavigation} ";
-                TakeScreenshot.SaveScreenshot();
-                throw new Exception(mainNavigationException);
             }
         }
 
