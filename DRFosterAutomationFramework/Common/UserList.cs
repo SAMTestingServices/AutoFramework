5d86babe8316efd4a7beb99b8c40eae4420c7bfb
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-21 16:03:15 +0000

    Corrected eMBED references after name change in app.config

--- DRFosterAutomationFramework/Common/UserList.cs
+++ DRFosterAutomationFramework/Common/UserList.cs
@@ -3,8 +3,8 @@
     public class UserList
     {
         //For use in HUB
-        public const string HUBAdminUsername = "AdminUsername";
-        public const string HUBAdminPassword = "AdminPassword";
+        public const string HUBAdminUsername = "HUBAdminUsername";
+        public const string HUBAdminPassword = "HUBAdminPassword";
 
         //For use in eMBED
         public const string EMBED005Username = "EMBED005Username";
