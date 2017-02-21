0b94d8e27d8b2b997199dd99d961895765d0a394
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-21 11:37:37 +0000

    Adding file to extend Selenium functionality

---
+++ DRFosterAutomationFramework/Common/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DRFosterAutomationFramework.Common
+{
+    public static class Extensions
+    {
+        private const int DefaultSleepBeforeRetrySpeed = 50;
+
+        public static void WaitForPageToLoad()
+        {
+            var timer = Stopwatch.StartNew();
+            Console.WriteLine("Enter WaitForPageToLoad - Time passed is: {0} milliseconds.", timer.Elapsed.Milliseconds);
+
+            for (int i = 0; i < 400; i++) // HACK: to slow it down, refactor
+            {
+                var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(60));
+                wait.Until((wdriver) => (wdriver as IJavaScriptExecutor).ExecuteScript("return document.readyState").Equals("complete"));
+            }
+
+            Console.WriteLine("Exit WaitForPageToLoad - Time passed is: {0} milliseconds.", timer.Elapsed.Milliseconds);
+        }
+    }
+}
