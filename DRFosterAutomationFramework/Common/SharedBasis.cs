0c810d45ff68eedb1364dc1b00640880ad07b72b
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-22 16:59:50 +0000

    Minor updates to address running tests in sequence, more to do

--- DRFosterAutomationFramework/Common/SharedBasis.cs
+++ DRFosterAutomationFramework/Common/SharedBasis.cs
@@ -24,7 +24,7 @@ namespace DRFosterAutomationFramework.Common
 
             // Driver.ClearCookies();
             // Driver.Close();
-            Driver.Quit();
+            // Driver.Quit();
         }
     }
 }
