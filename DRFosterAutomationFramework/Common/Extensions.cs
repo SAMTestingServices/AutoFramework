4abc739cf6684a69e146ab7d1fe020d5575974d7
Mark Crowther <mcrowther@dr-foster.lan>
2017-02-22 16:58:56 +0000

    Minor change to layout for readability

--- DRFosterAutomationFramework/Common/Extensions.cs
+++ DRFosterAutomationFramework/Common/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
@@ -13,16 +12,13 @@ namespace DRFosterAutomationFramework.Common
 
         public static void WaitForPageToLoad()
         {
-            var timer = Stopwatch.StartNew();
-            Console.WriteLine("Enter WaitForPageToLoad - Time passed is: {0} milliseconds.", timer.Elapsed.Milliseconds);
-
             for (int i = 0; i < 400; i++) // HACK: Refactor WaitForPageToLoad as this is essentially an explicit wait
             {
                 var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(60));
-                wait.Until((wdriver) => (wdriver as IJavaScriptExecutor).ExecuteScript("return document.readyState").Equals("complete"));
+                wait.Until((wdriver) => (wdriver as IJavaScriptExecutor)
+                .ExecuteScript("return document.readyState")
+                .Equals("complete"));
             }
-
-            Console.WriteLine("Exit WaitForPageToLoad - Time passed is: {0} milliseconds.", timer.Elapsed.Milliseconds);
         }
     }
 }
